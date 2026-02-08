10d989d3ed3ecced215ab65ed1659d7b0caa1957
Ann-Getit <Ann-Getit+182119360+Ann-Getit@users.noreply.github.com>
2026-02-08 16:03:54 +0100

    .

--- api/vertrektijden.js
+++ api/vertrektijden.js
@@ -1,10 +1,5 @@
 export default async function handler(req, res) {
-  try {
-    const { station } = req.query;
 
-    if (!station) {
-      return res.status(400).json({ error: "station parameter is required" });
-    }
 
     const response = await fetch(
       `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures`,
@@ -19,7 +14,5 @@ export default async function handler(req, res) {
 
     res.status(200).json(vertrektijden.payload || vertrektijden);
 
-  } catch (error) {
-    res.status(500).json({ error: error.toString() });
   }
-}
+
