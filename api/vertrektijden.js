19ab73e2da1537da6407437e2e200cafd5207e51
Ann-Getit <Ann-Getit+182119360+Ann-Getit@users.noreply.github.com>
2026-02-08 21:26:12 +0100

    m

--- api/vertrektijden.js
+++ api/vertrektijden.js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
   try {
-    const { station } = req.query;
-
+    const { station } = req.query; //pakt elke station naam uit elk URL data
+    console.log(station);
+    //als er geen stations zijn je krijg error. return zorgt eroor dat het niet meer actief blijft
     if (!station) {
       return res.status(400).json({ error: "station parameter is required" });
     }
 
+    //http request naar ns api met await fetch
     const response = await fetch(
       `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?station=${station}`,
       {
@@ -15,11 +17,11 @@ export default async function handler(req, res) {
       }
     );
 
-    const vertrektijden = await response.json();
+    const vertrektijden = await response.json(); //converteert naar json formaat
+    //vertrektijden is js object met alle data van ns api
+    console.log(vertrektijden);
 
-    
-
-    res.status(200).json(vertrektijden.departures || []);
+    res.status(200).json(vertrektijden.Departures || []); //als DEPARTURES hiet bestaat geef lege array !
 
   } catch (error) {
     res.status(500).json({ error: error.toString() });
