63a394ca4478dc84f4b01a9418cb33758381388a
Ann-Getit <Ann-Getit+182119360+Ann-Getit@users.noreply.github.com>
2026-02-09 18:40:50 +0100

    "projects" links aangepast

--- src/assets/Nav.jsx
+++ src/assets/Nav.jsx
@@ -1,30 +1,42 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "../assets/Nav.css";
 
 export default function Nav() {
 const [open, setOpen] = useState(false);
+const [setWorkOpen] = useState(false);
+const navRef = useRef(null);
+
+
+useEffect(() => {
+  const handleClickOutside = (event) => {
+    if (navRef.current && !navRef.current.contains(event.target)) {
+      setWorkOpen(false);
+    }
+  };
+
+  document.addEventListener("mousedown", handleClickOutside);
+  return () => document.removeEventListener("mousedown", handleClickOutside);
+}, []);
+
+
   return (
     <>
-    <nav className="nav-container">
+    <nav ref={navRef} className="nav-container">
 
         <div className="logo">A.developer</div>
+
 
         {/* Desktop links */}
       <ul className="nav-links desktop">
           <li><Link to="#home">Home</Link></li> {/** inplaats van anchor href toch wel link to gebruiken zodat ik niet vast blijft in "project page" */}
           <li><Link to="#about">About</Link></li> {/** hover voor de nav links */}
           
-          <li className="dropdown">
-              <span className="dropbtn">Work ▾</span>
-              <ul className="dropdown-content">
-                <li><Link to="/project">Projects</Link></li> 
-             </ul>
-          </li> 
+          <li><Link to="/project" className="changebtn">Projects</Link></li> 
+         
           <li><Link to="#contact">Contact</Link></li>
-        </ul>
-
+       </ul>
         {/* Hamburger voor mobiel */} 
       <div className="hamburger" onClick={() => setOpen(!open)}>
         &#9776; 
@@ -32,19 +44,19 @@ const [open, setOpen] = useState(false);
 
 
 
+
+
+
+
 {/* Mobile overlay */}
       <ul className={`nav-links mobile ${open ? "open" : ""}`}>
         <li><Link to="#home" onClick={() => setOpen(false)}>Home</Link></li>
         <li><Link to="#about" onClick={() => setOpen(false)}>About</Link></li>
-        <li className="dropdown">
-          <span className="dropbtn">Work ▾ </span>
-          <ul className="dropdown-content">
         <li><Link to="/project" onClick={() => setOpen(false)}>Projects</Link></li>
-        </ul>
-        </li>
-        
         <li><Link to="#contact" onClick={() => setOpen(false)}>Contact</Link></li>
       </ul>
+
+
 
       {/* overlay achtergrond bij mobiel */}
        {open && <div className="overlay" onClick={() => setOpen(false)} />}
@@ -57,5 +69,8 @@ const [open, setOpen] = useState(false);
     </>
   );
 }
+
+
+
 
 
